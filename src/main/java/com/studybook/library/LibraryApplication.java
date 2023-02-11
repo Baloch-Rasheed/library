b7fe67b710518f7000d8c89324d287b1764e5f23
Baloch Rasheed <73610218+Baloch-Rasheed@users.noreply.github.com>
2023-02-11 23:00:24 +0500

    Stage-One

---
+++ src/main/java/com/studybook/library/LibraryApplication.java
@@ -0,0 +1,13 @@
+package com.studybook.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryApplication.class, args);
+	}
+
+}
